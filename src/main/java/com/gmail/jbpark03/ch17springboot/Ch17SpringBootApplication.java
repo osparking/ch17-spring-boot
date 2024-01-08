48b6de0599b3091d290323275c03a7b1991e6361
osparking <jbpark03@gmail.com>
2024-01-08 14:27:54 +0900

    웹 initializr 를 사용하여 생성한 메이븐 프로젝트

---
+++ src/main/java/com/gmail/jbpark03/ch17springboot/Ch17SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.jbpark03.ch17springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch17SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch17SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/jbpark03/ch17springboot/Ch17SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.jbpark03.ch17springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch17SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
