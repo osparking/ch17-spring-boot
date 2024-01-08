4ac019950fb6dc0db610ddf314e864dadb4694ae
osparking <jbpark03@gmail.com>
2024-01-08 19:26:05 +0900

    애노'가 교체된 봄부트 시험 클래스

---
+++ src/test/java/com/gmail/jbpark03/ch17springboot/RegistrationTest.java
@@ -0,0 +1,36 @@
+package com.gmail.jbpark03.ch17springboot;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.gmail.jbpark03.ch17springboot.model.Passenger;
+import com.gmail.jbpark03.ch17springboot.regis.PassengerRegistrationEvent;
+import com.gmail.jbpark03.ch17springboot.regis.RegistrationManager;
+
+@SpringBootTest
+@EnableAutoConfiguration
+@ImportResource("classpath:application-context.xml")
+class RegistrationTest {
+
+  @Autowired
+  private RegistrationManager manager;
+
+  @Autowired
+  private Passenger passenger;
+
+  @Test
+  void test() {
+    var event = new PassengerRegistrationEvent(passenger);
+    System.out.println("사건 발생 후:" + passenger);
+    manager.getApplicationContext().publishEvent(event);
+    assertTrue(passenger.isRegistered());
+  }
+}
